157c43060cc6d8f54ce7df48e88a48ec39308eec
Isaiah Raya <rayaisaiah@gmail.com>
2016-09-23 13:40:54 -0700

    added time

--- chatbot.js
+++ chatbot.js
@@ -1,30 +1,36 @@
 var response = {
-"Hi": "Hola!",
-"Yo": "Whats up!",
-"Hit me up with the code Dave!" : "I got you - Up,Up,Down,Down,Left,Right,A,B!",
-"How old are you?" : "I was created at the begining of time.",
-"What is your name?" : "I am DAVE the chatbot!",
-"What time is it?" : "My creator has not programmed that yet!",
-"Whats today?" : "n"
+"hi": "Hola! (Hi)",
+"hello": "Hola! (Hi)",
+"yo": "Whats up?",
+"whats up": "The sky",
+"hit me up with the code Dave!" : "I got you - Up,Up,Down,Down,Left,Right,Left,Right,A,B!",
+"how old are you" : "I was created at the begining of time.",
+"what is your name" : "I am DAVE the chatbot!",
+"why are you stupid" : "Why were you born? ** Dave the SavageBot! **",
+"what time is it" : "The time is " + returnTime() + " of the clock",
+"whats today" : "n",
+"" : "..."
+
 }
 
 function chatBotResponse(){
-var question =  document.getElementById("input").value;
+var question =  document.getElementById("input").value.toLowerCase();
 var answer = response[question];
-$('#chat-area').prepend("Dave: " + answer + "</br>");
-$('#chat-area').prepend("You: " + question + "." + "</br>");
 
+
+if (answer === null || answer === undefined){
+  $('#chat-area').prepend("Dave: I dont feel like answering that now..." + "</br>");
+    $('#chat-area').prepend("You: " + question + "." + "</br>");
 }
-
-//var d = new Date();
-//var weekday = new Array();
-//weekday[0] = "Sunday";
-//weekday[1] = "Monday";
-//weekday[2] = "Tuesday";
-//weekday[3] = "Wednesday";
-//weekday[4] = "Thursday";
-//weekday[5] = "Friday";
-//weekday[6] = "Saturday";
-
-//var n = weekday[d.getUTCDay()];
-//obj[n] = n;
+else {
+  $('#chat-area').prepend("Dave: " + answer + "</br>");
+  $('#chat-area').prepend("You: " + question + "." + "</br>");
+}
+}
+function returnTime() {
+var d = new Date(Date.now());
+var hours = d.getHours();
+var minutes = d.getMinutes();
+var n = hours + ":" + minutes;
+return n;
+}

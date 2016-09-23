51c358c8f5d35b08dc8b5c02e2b08282453db743
Isaiah Raya <rayaisaiah@gmail.com>
2016-09-23 08:17:27 -0700

    Added prepend!

--- chatbot.js
+++ chatbot.js
@@ -1,7 +1,7 @@
 var response = {
 "Hi": "Hola!",
 "Yo": "Whats up!",
-"Hit me up with the code Dave!" : "I got you- Up,Up,Down,Down,Left,Right,A,B!",
+"Hit me up with the code Dave!" : "I got you - Up,Up,Down,Down,Left,Right,A,B!",
 "How old are you?" : "I was created at the begining of time.",
 "What is your name?" : "I am DAVE the chatbot!",
 "What time is it?" : "My creator has not programmed that yet!",
@@ -10,21 +10,21 @@ var response = {
 
 function chatBotResponse(){
 var question =  document.getElementById("input").value;
-var answer = "Dave: " + response[question];
-
-document.getElementById("chat-area").textContent = answer;
+var answer = response[question];
+$('#chat-area').prepend("Dave: " + answer + "</br>");
+$('#chat-area').prepend("You: " + question + "." + "</br>");
 
 }
 
-var d = new Date();
-var weekday = new Array();
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
+//var d = new Date();
+//var weekday = new Array();
+//weekday[0] = "Sunday";
+//weekday[1] = "Monday";
+//weekday[2] = "Tuesday";
+//weekday[3] = "Wednesday";
+//weekday[4] = "Thursday";
+//weekday[5] = "Friday";
+//weekday[6] = "Saturday";
 
-var n = weekday[d.getUTCDay()];
-obj[n] = n;
+//var n = weekday[d.getUTCDay()];
+//obj[n] = n;

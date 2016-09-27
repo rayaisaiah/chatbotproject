3f045375c85fe01ef8f9ad2a61083688ac080097
Isaiah Raya <rayaisaiah@gmail.com>
2016-09-27 13:29:23 -0700

    added regex

--- chatbot.js
+++ chatbot.js
@@ -1,14 +1,14 @@
 
 var multimemes = ["<img src='http://i2.mirror.co.uk/incoming/article8075004.ece/ALTERNATES/s615b/Harambe.jpg' height='42' width='42'>",
-"<img src='http://images.mentalfloss.com/sites/default/files/styles/article_640x430/public/catffaceheader.jpg' height='42' width='42'>",
-"<img src='https://i.imgflip.com/8h0c8.jpg' height='42' width='42'>"];
+"<img src='http://images.mentalfloss.com/sites/default/files/styles/article_640x430/public/catffaceheader.jpg' height='42' width='42'>"];
+
 var multianswers = ["Dave: I dont feel like answering that now...", "not now!","blah!"];
 var response = {
 "hi": "Hola! (Hi)",
 "hello": "Hola! (Hi)",
 "yo": "Whats up?",
 "whats up": "The sky",
-"hit me up with the code dave!" : "I got you - Up,Up,Down,Down,Left,Right,Left,Right,B,A!",
+"hit me up with the code dave" : "I got you - Up,Up,Down,Down,Left,Right,Left,Right,B,A!",
 "how old are you" : "I was created at the beggining of time.",
 "what is your name" : "I am DAVE the chatbot!",
 "why are you stupid" : "Why were you born? ** Dave the SavageBot! **",
@@ -17,24 +17,31 @@ var response = {
 "" : "...",
 "yooo" : multianswers[Math.floor((Math.random() * multianswers.length))],
 "picture" : "<img src='http://images.mentalfloss.com/sites/default/files/styles/article_640x430/public/catffaceheader.jpg' height='42' width='42'>",
-"hit me up with a spicy meme" : multimemes[Math.floor((Math.random() * multianswers.length))],
-"play me some jams" : "not not m8... not now."
+"hit me up with a spicy meme" : multimemes[Math.floor((Math.random() * multianswers.length))]
+
 }
 
 function chatBotResponse(){
 var question =  document.getElementById("input").value.toLowerCase();
+question = question.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, '');
 var answer = response[question];
 
 
 if (answer === null || answer === undefined){
   $('#chat-area').prepend("<div style='color: yellow'>" + "Dave: I dont feel like answering that now..." + "</br>");
-    $('#chat-area').prepend("You: " + question + "." + "</br>");
+  $('#chat-area').prepend("You: " + question + "." + "</br>");
 }
 else {
   $('#chat-area').prepend("<div style='color: yellow'>" + "Dave: " + answer + "</br>");
   $('#chat-area').prepend("You: " + question + "." + "</br>");
 }
 }
+
+function chatBotQuestion() {
+
+
+}
+
 function returnTime() {
 var d = new Date(Date.now());
 var hours = d.getHours();

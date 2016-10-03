ef8d85b0935502010e9076b5701a01740dd0a372
Isaiah Raya <rayaisaiah@gmail.com>
2016-10-03 13:38:41 -0700

    answer changes

--- chatbot.js
+++ chatbot.js
@@ -1,6 +1,5 @@
 
-var multimemes = [//"<img src='http://i2.mirror.co.uk/incoming/article8075004.ece/ALTERNATES/s615b/Harambe.jpg' height='42' width='42'>",
-"<img src='http://images.mentalfloss.com/sites/default/files/styles/article_640x430/public/catffaceheader.jpg' height='42' width='42'>"];
+var multimemes = ["<img src='http://images.mentalfloss.com/sites/default/files/styles/article_640x430/public/catffaceheader.jpg' height='42' width='42'>"];
 
 var multianswers = ["Dave: I dont feel like answering that now...", "Not now!","Blah!"];
 var response = {
@@ -9,11 +8,10 @@ var response = {
 "yo": "Whats up?",
 "whats up": "The sky",
 "hit me up with the code dave" : "I got you - Up,Up,Down,Down,Left,Right,Left,Right,B,A!",
-"how old are you" : "I was created at the beggining of time.",
+"how old are you" : "I was created at the begining of time.",
 "what is your name" : "I am DAVE the chatbot!",
 "why are you stupid" : "Why were you born? ** Dave the SavageBot! **",
 "what time is it" : "The time is " + returnTime() + ".",
-"whats today" : "n",
 "" : "...",
 "yooo" : multianswers[Math.floor((Math.random() * multianswers.length))],
 "picture" : "<img src='http://images.mentalfloss.com/sites/default/files/styles/article_640x430/public/catffaceheader.jpg' height='42' width='42'>",
@@ -45,6 +43,18 @@ var daveQuestion = questions;
 
 $('#chat-area').prepend("<div style='color: yellow'>" + "Dave: " + questions[Math.floor((Math.random() * multianswers.length))] + "?" + "</br>");
 }
+//function chatBotWords(){
+  //var question = document.getElementById("input").value.toLowerCase();
+  //var questionArray = question.split(" ");
+  //var i;
+  //for (i = 0; i < questionArray.length; i++) {
+    // questionArray = {
+      //"hi" : "yo"
+    //}
+    //$('#chat-area').prepend("You: " + questionArray + "</br>");
+//}
+//}
+
 var responseQ = {
 "good" : "Awesome!",
 "bad" : "Life has its ups and downs.",
@@ -53,6 +63,7 @@ var responseQ = {
 "" : "...",
 "great" : "Awesome!",
 "hi" : "Hello again."
+"ok" : "Okay."
 }
 
 function chatBotQuestionResponse(){
